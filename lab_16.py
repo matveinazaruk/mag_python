4b4c1dfa2377eb07d03e380eebc34389a4a2d696
Matvei <matvei.nazaruk@gmail.com>
2016-12-14 14:37:14 +0300

    Update lab_16.py

--- lab_16.py
+++ lab_16.py
@@ -6,14 +6,22 @@ def x_range( *args):
 	elif len(args) == 3:
 		start, stop, step = args
 	else:
-		raise TypeError('xrange() requires 1-3 int arguments')
+		raise TypeError('x_range() requires 1-3 int arguments')
+
+	if step == 0:
+		raise ValueError('x_range() arg 3 must not be zero')
 
 	current = start
 	while (step > 0 and current < stop) or (step < 0 and current > stop):
 		yield current
 		current += step
 
+#Correct
 assert list(x_range(1, 5, 2)) == list(range(1, 5, 2))
-assert list(x_range(5, 1, -2)) == list(range(5, 1, -2))
+assert list(x_range(1, 5, -2)) == list(range(1, 5, -2))
 assert list(x_range(1, 5)) == list(range(1, 5))
 assert list(x_range(5)) == list(range(5))
+
+#Incorrect
+assert list(x_range(1, 5, -2)) == list(range(1, 5, -2))
+assert list(x_range(5, 1)) == list(range(5, 1))
